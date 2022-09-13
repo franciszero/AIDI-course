d42328701e8cc9727a4ea443e92f28a82a16a602
chenfangji1 <chenfangji1@360.com>
2022-09-12 23:23:48 -0400

    INIT

---
+++ AI for Business Decision Making/Assign1 - Red wine/solution/main.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import os
+import shutil
+import time
+import math
+import argparse
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    workspace = "/Users/francis/Documents/Georgian College/AIDI/AI for Business Decision Making/Assign1 - Red wine/solution"
+    path = workspace + "/data/winequality-red.csv"
+
+    df = pd.read_csv(path)
+
+    titanic = sns.load_dataset('titanic')
+    iris = sns.load_dataset('iris')
+
+    # fig = sns.pairplot(df)
+    # fig.savefig(workspace + "/output/1.png")
+
+    fig = sns.pairplot(df, hue="quality", plot_kws = {'alpha': 0.7})
+    # fig.map_lower(sns.kdeplot, levels=3)
+    fig.savefig(workspace + "/output/2.png", dpi=400)
+
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ Machine Learning Programming/Week1 exercise/Exercise2.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+from __future__ import print_function
+
+import time
+import pandas as pd
+from scipy.sparse import lil_matrix
+import matplotlib.pyplot as plt
+
+import math
+import sys
+
+
+class car:
+    def __init__(self, year, mpg, speed):
+        self.year = year
+        self.mpg = mpg
+        self.speed = speed
+        self.owners = []
+
+    def accelerate(self):
+        self.speed += 30
+
+    def brake(self):
+        self.speed -= 60
+        if self.speed < 0:
+            self.speed = 0
+
+    def __str__(self):
+        return "car year {} mpg {} speed {}.".format(self.year, self.mpg, self.speed)
+
+
+def func():
+    car1 = car(2016, 20, 80)
+    print(car1)
+    car2 = car(2013, 25, 60)
+    print(car2)
+
+
+if __name__ == '__main__':
+    func()
