31ca9fc29f524ad7a31e75569a113b3377c0b548
Francis Chan <franciszero@gmail.com>
2022-09-18 19:25:42 -0400

    .

--- Conversational AI/Week2 Exercise1/app.py
+++ Conversational AI/Week2 Exercise1/app.py
@@ -17,7 +17,27 @@ def foo():
 def webpage():
     return render_template("1.html")
 
+
 #
 # @app.getdocpic("/getdog")
 # def getdog():
 #     return render_template("1.html")
+
+
+# see: https://github.com/rbigelow/flask-intro/blob/main/app.py
+@app.route("/ross")
+def ross():
+    return "<p> Hi I'm Ross. </p><img src='https://bigelow.cc/wp-content/uploads/sites/3/cropped-ross-bigelow.png'> "
+
+
+myData = {
+    "name": 'Ross Bigelow',
+    "dream": "To inspire others to fulfil thier dreams.",
+    'age': 1,
+    'birthdate': "2020-01-01"
+}
+
+
+@app.route("/json")
+def myJson():
+    return myData
