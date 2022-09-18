dde7bf4ad5f834eb2c6e1ac63bb4d5a2df4b5dcd
Francis Chan <franciszero@gmail.com>
2022-09-18 19:20:44 -0400

    .

--- AI for Business Decision Making/Assign1 - Red wine/solution/main.py
+++ AI for Business Decision Making/Assign1 - Red wine/solution/main.py
@@ -1,9 +1,4 @@
 # coding=utf-8
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 import os
 import shutil
 import time
@@ -14,11 +9,6 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 # %matplotlib inline
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
@@ -37,7 +27,3 @@ if __name__ == '__main__':
     # fig = sns.pairplot(df, hue="quality", plot_kws = {'alpha': 0.7})
     # fig.map_lower(sns.kdeplot, levels=3)
     # fig.savefig(workspace + "/output/2.png", dpi=400)
-
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
