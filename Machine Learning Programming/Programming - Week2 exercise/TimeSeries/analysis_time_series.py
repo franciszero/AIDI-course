f02a6e491fd456f1eb04e5387843ee8fc894bb0e
Francis Chan <franciszero@gmail.com>
2022-09-24 00:27:12 -0400

    .

--- Machine Learning Programming/Programming - Week2 exercise/TimeSeries/analysis_time_series.py
+++ Machine Learning Programming/Programming - Week2 exercise/TimeSeries/analysis_time_series.py
@@ -1,26 +1,21 @@
 # coding=utf-8
 import pandas as pd
-from sklearn.covariance import EllipticEnvelope
-from sklearn.model_selection import GridSearchCV
-from sklearn.neighbors import LocalOutlierFactor
-from statsmodels.tsa.seasonal import seasonal_decompose
-import matplotlib.pyplot as plt
-import seaborn as sns
-import math
 import numpy as np
-from scipy import stats
-import statsmodels.api as sm
-from numpy import asarray
-from numpy import exp
-from matplotlib import pyplot
-from sklearn.neighbors import KernelDensity
-from scipy.stats import norm
+import math
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KernelDensity
+from statsmodels.tsa.seasonal import seasonal_decompose
 from TimeSeries.KDE_exercise import my_scores
 import mplfinance as mpf
-import talib
-# import psycopg2 as pg
-import matplotlib.gridspec as gridspec
+
+import matplotlib
+from decimal import Decimal
+
+matplotlib.rc('font', size=5)
 
 
 class TimeSerious:
@@ -134,7 +129,7 @@ class TimeSerious:
         self.plot_volume_and_its_anom(ax_volume, residual_anomalies, x)
         # plt.setp(baseline2, linewidth=1, color='yellow', markersize=10)
         # save fig
-        plt.suptitle("Anomalies defined by TS Residual Distribution")
+        plt.suptitle("Anomalies defined by TS Residual Distribution", fontsize=12)
         fig.savefig("./output/job3_visualize_the_volume_outliers.png")
         return
 
@@ -153,31 +148,58 @@ class TimeSerious:
 
         # https://github.com/matplotlib/mplfinance/blob/master/examples/marketcolor_overrides.ipynb
         # https://github.com/matplotlib/mplfinance/blob/master/examples/savefig.ipynb
-        mpf.plot(d, volume=True, style='yahoo', type='line', savefig=path, title=title)  # type='candle'
+        mpf.plot(d, volume=True, style='yahoo', type='candle', savefig=path, title=title, warn_too_much_data=10000)
+
+    def countDivisors(self, num):
+        ans = 1
+        x = 2
+        while x * x <= num:
+            cnt = 1
+            while num % x == 0:
+                cnt += 1
+                num /= x
+            ans *= cnt
+            x += 1
+        return ans * (1 + (num > 1))
+
+    def get_tick_labels(self, x_max, cnt):
+        arr = np.linspace(0, x_max, num=cnt, endpoint=True)
+        steps = float("%.0e" % arr[1])
+        return np.arange(0, x_max + steps, steps)
 
     def plot_KDE(self, ax1, dist, l, m, r, x, x_test, y):
-        # prepare twin plot ax
-        ax2 = ax1.twiny()
-        [t.set_color('red') for t in ax1.xaxis.get_ticklabels()]
-        [t.set_color('skyblue') for t in ax2.xaxis.get_ticklabels()]
         # histogram of residuals
-        ax2 = plt.hist(y, 80, color='skyblue', histtype='stepfilled', orientation='horizontal', alpha=0.8)
+        # ax1.title.set_text('KDE')
+        ax1.set_title('KDE', fontdict={'fontsize': 9})
+        ax1.hist(y, 80, histtype='stepfilled', orientation='horizontal', alpha=0.8)
+        # [label.set_fontsize(7) for label in (ax1.get_xticklabels() + ax1.get_yticklabels())]
+        [tick.set_color('steelblue') for tick in ax1.xaxis.get_ticklabels()]
         # KDE
-        ax1.plot(dist, x_test, '-.', color='r', linewidth=.8, alpha=1)
-        ax1.axis(xmin=0, xmax=max(dist) * 1.1)
-        # plot anomalies threshold in residuals
-        ax1.plot(x, [l] * len(x), color='gray', linewidth=.2, alpha=.8)
-        ax1.plot(x, [r] * len(x), color='gray', linewidth=.2, alpha=.8)
-        ax1.plot(x, [m] * len(x), color='red', linewidth=.9, alpha=1)
-        ax1.title.set_text('KDE')
+        ax2 = ax1.twiny()
+        ax2.plot(dist, x_test, '-.', color='r', linewidth=.8, alpha=1)
+        labels = self.get_tick_labels(max(dist), 5)
+        ticks = np.arange(len(labels))
+        ax2.axis(xmin=0, xmax=labels[-1])
+        # ax2.set_xticks(ticks, minor=False)
+        # ax2.set_xticklabels(labels, fontdict=None, minor=False)
+
+        [tick.set_color('red') for tick in ax2.xaxis.get_ticklabels()]
+        # [label.set_fontsize(7) for label in (ax2.get_xticklabels())]
+        # plot threshold in residuals
+        ax2.plot(x, [l] * len(x), color='gray', linewidth=.2, alpha=.8)
+        ax2.plot(x, [r] * len(x), color='gray', linewidth=.2, alpha=.8)
+        ax2.plot(x, [m] * len(x), color='red', linewidth=.9, alpha=1)
+        # ax1.xaxis.set_major_formatter(mticker.FormatStrFormatter('%.0f°E'))
+        ax1.xaxis.set_major_formatter(FormatStrFormatter('%.0f'))
 
     def plot_volume_and_its_anom(self, ax_volume, residual_anomalies, x):
+        # ax_volume.title.set_text('volume')
+        ax_volume.set_title('volume', fontdict={'fontsize': 9})
         # get volume anom
         a = pd.concat([self.df['Volume'], residual_anomalies], axis=1, ignore_index=False)
         a['anomalies'].fillna(0, inplace=True)  # a.loc[a['anomalies'] != a['anomalies'], 'Volume'] = 0
         ax_volume.axis(ymin=0, ymax=max(self.df['Volume']) * 1.1)
         ax_volume.axis(xmin=0, xmax=len(self.df['Volume']))
-        ax_volume.title.set_text('volume')
         # plot volume anom
         anom_x = a[a['anomalies'] != 0.0].index.values
         anom_y = a[a['anomalies'] != 0.0]['Volume'].values
@@ -187,8 +209,12 @@ class TimeSerious:
         plt.setp(baseline, visible=False)
         # plot volume
         ax_volume.scatter(x, self.df['Volume'], s=4, alpha=.8, c="skyblue", edgecolors='gray', linewidths=.2)
+        ax_volume.ticklabel_format(style='sci', scilimits=(-1, 2), axis='y')
+        [label.set_fontsize(6) for label in (ax_volume.get_xticklabels() + ax_volume.get_yticklabels())]
 
     def plot_resid_and_its_anom(self, ax_resid, l, m, r, residual, residual_anomalies, x, y):
+        # ax_resid.title.set_text('volume residual')
+        ax_resid.set_title('volume residual', fontdict={'fontsize': 9})
         # plot residuals
         ax_resid.scatter(x, y, s=4, alpha=.8, c="skyblue", edgecolors='gray', linewidths=.2)
         # plot residuals anom
@@ -203,7 +229,7 @@ class TimeSerious:
         ax_resid.plot(x, [r] * len(x), color='gray', linewidth=.2, alpha=.8)
         # reset x axis
         ax_resid.axis(xmin=0, xmax=len(y))
-        ax_resid.title.set_text('volume residual')
+        [label.set_fontsize(6) for label in (ax_resid.get_xticklabels() + ax_resid.get_yticklabels())]
 
     def my_scores(self, estimator, X):
         scores = estimator.score_samples(X)
