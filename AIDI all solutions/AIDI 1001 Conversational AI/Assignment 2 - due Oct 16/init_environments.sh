26c5e074e339341a731f84062336bc1bda37d489
Francis Chan <franciszero@gmail.com>
2022-10-09 17:26:56 -0400

    .

--- AIDI all solutions/AIDI 1001 Conversational AI/Assignment 2 - due Oct 16/init_environments.sh
+++ AIDI all solutions/AIDI 1001 Conversational AI/Assignment 2 - due Oct 16/init_environments.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-pip list --format=freeze | \
-awk -F '==' '{if($1!="wheel" || $1!="pip" || $1!="distributed" || $1!="setuptools"){print $0}}' \
-> requirements.txt
+pip list --format=freeze | awk -F '==' '{if($1!~/^conda*/ && $1!~/^anaconda*/ && $1!="wheel" && $1!="pip" && $1!="distributed" && $1!="setuptools"){print $0}}' > requirements.txt
