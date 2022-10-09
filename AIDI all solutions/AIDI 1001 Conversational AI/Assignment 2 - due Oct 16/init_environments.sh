3f5f80297091f9f6e381c3f6316921766e543551
Francis Chan <franciszero@gmail.com>
2022-10-09 18:48:46 -0400

    .

--- AIDI all solutions/AIDI 1001 Conversational AI/Assignment 2 - due Oct 16/init_environments.sh
+++ AIDI all solutions/AIDI 1001 Conversational AI/Assignment 2 - due Oct 16/init_environments.sh
@@ -2,6 +2,10 @@
 
 pip list --format=freeze | awk -F '==' '
 {if($1!~/^en-core-web/ &&
+$1!="tables" &&
+$1!="numba" &&
+$1!~/mkl-/ &&
+$1!="llvmlite" &&
 $1!="idna" &&
 $1!="navigator-updater" &&
 $1!="pyodbc" &&
@@ -11,4 +15,11 @@ $1!="seaborn" &&
 $1!="TBB" &&
 $1!="typer" &&
 $1!="clyent" &&
-$1!="Bottleneck" && $1!~/^numpy*/ && $1!~/^conda*/ && $1!~/^anaconda*/ && $1!="wheel" && $1!="pip" && $1!="distributed" && $1!="setuptools"){print $0}}' > requirements.txt
+$1!="Bottleneck" &&
+$1!~/^numpy*/ &&
+$1!~/^conda*/ &&
+$1!~/^anaconda*/ &&
+$1!="wheel" &&
+$1!="pip" &&
+$1!="distributed" &&
+$1!="setuptools"){print $0}}' > requirements.txt
