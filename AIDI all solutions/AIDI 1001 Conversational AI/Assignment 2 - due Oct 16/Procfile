7cc02124589ecba7e3882d57e44f5773292c5598
Francis Chan <franciszero@gmail.com>
2022-10-10 18:15:16 -0400

    .

--- AIDI all solutions/AIDI 1001 Conversational AI/Assignment 2 - due Oct 16/Procfile
+++ AIDI all solutions/AIDI 1001 Conversational AI/Assignment 2 - due Oct 16/Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:flapp
+web: gunicorn app:app
--- AIDI all solutions/AIDI 1001 Conversational AI/Assignment 2 - due Oct 16/init_environments.sh
+++ AIDI all solutions/AIDI 1001 Conversational AI/Assignment 2 - due Oct 16/init_environments.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
 
-pip list --format=freeze | awk -F '==' '
-{if($1!~/^en-core-web/ &&
-$1!="tables" &&
-$1!="numba" &&
-$1!~/mkl-/ &&
-$1!="llvmlite" &&
-$1!="idna" &&
-$1!="navigator-updater" &&
-$1!="pyodbc" &&
-$1!="ruamel-yaml-conda" &&
-$1!="scikit-learn-intelex" &&
-$1!="seaborn" &&
-$1!="TBB" &&
-$1!="typer" &&
-$1!="clyent" &&
-$1!="Bottleneck" &&
-$1!~/^numpy*/ &&
-$1!~/^conda*/ &&
-$1!~/^anaconda*/ &&
-$1!="wheel" &&
-$1!="pip" &&
-$1!="distributed" &&
-$1!="setuptools"){print $0}}' > requirements.txt
+pip freeze > requirements.txt
