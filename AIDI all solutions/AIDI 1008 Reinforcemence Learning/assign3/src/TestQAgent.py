2092c00adc8da320edf0b97eb4b1af7d54f1452c
Francis Chan <franciszero@gmail.com>
2023-04-06 16:34:32 -0400

    .

--- AIDI all solutions/AIDI 1008 Reinforcemence Learning/assign3/src/TestQAgent.py
+++ AIDI all solutions/AIDI 1008 Reinforcemence Learning/assign3/src/TestQAgent.py
@@ -21,29 +21,29 @@ import tensorflow as tf
 from QAgent import QAgent
 from Environment import EnvCartPole
 
+# train agent1
 env = EnvCartPole(discrete_base=8, new_step_api=False)
-agent1 = QAgent(env, n_episodes=500, n_steps=1000, gamma=0.99, alpha=0.5, epsilon=1, )
+agent1 = QAgent(env, n_episodes=100, n_steps=1000, gamma=0.99, alpha=0.5, epsilon=1, )
 cp1 = "policy1"
 agent1.run(checkpoint_name=cp1, new_r=False)
 agent1.visualization()
 
+# train agent2
 env = EnvCartPole(discrete_base=8, new_step_api=False)
-agent2 = QAgent(env, n_episodes=500, n_steps=1000, gamma=0.99, alpha=0.5, epsilon=None,
+agent2 = QAgent(env, n_episodes=100, n_steps=1000, gamma=0.99, alpha=0.5, epsilon=None,
                 epsilon_start=0.5, epsilon_end=0.0001, epsilon_decay=1.0e+04, )
 cp2 = "policy2"
 agent2.run(checkpoint_name=cp2, new_r=False)
 agent2.visualization()
-agent2.n_episodes = 100
-agent2.test(checkpoint_name=cp2, new_r=False)
-agent2.visualization()
-test2 = agent2.steps
 
-
+# test agent1+agent2
 agent1.n_episodes = 100
-agent1.test(checkpoint_name=cp1, new_r=False)
-agent1.visualization()
-test1 = agent1.steps
-# df = pd.DataFrame({"random": test1[-1000:], "smart": test2[-1000:]})
-# df = df.stack().reset_index()
-# df.columns = ["x", "hue", "y"]
-# agent2.visualization(outside_df=df)
+test1 = agent1.test(checkpoint_name=cp1, new_r=False)
+agent2.n_episodes = 100
+test2 = agent2.test(checkpoint_name=cp2, new_r=False)
+
+# plot and compare them
+df = pd.DataFrame({"random": test1[-1000:], "smart": test2[-1000:]})
+df = df.stack().reset_index()
+df.columns = ["x", "hue", "y"]
+agent2.visualization(outside_df=df)
