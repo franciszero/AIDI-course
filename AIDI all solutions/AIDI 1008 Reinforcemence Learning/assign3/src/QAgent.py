2bec3671fb81b9c1e9699cae9931fac6a8f29c69
Francis Chan <franciszero@gmail.com>
2023-04-07 17:16:20 -0400

    .

--- AIDI all solutions/AIDI 1008 Reinforcemence Learning/assign3/src/QAgent.py
+++ AIDI all solutions/AIDI 1008 Reinforcemence Learning/assign3/src/QAgent.py
@@ -2,7 +2,9 @@ import numpy as np
 from abc import ABC, abstractmethod
 import math
 from itertools import count
+import pandas as pd
 from Agent import Agent
+from Environment import EnvCartPole
 
 
 class QAgent(Agent, ABC):
@@ -60,3 +62,34 @@ class QAgent(Agent, ABC):
                     self.steps.append(step + 1)
                     break
         return self.steps
+
+
+if __name__ == '__main__':
+    # train agent1
+    env = EnvCartPole(discrete_base=8, new_step_api=False)
+    agent1 = QAgent(env, n_episodes=300, n_steps=1000, gamma=0.99, alpha=0.5, epsilon=1, )
+    cp1 = "policy1"
+    agent1.run(new_r=False)
+    agent1.visualization()
+
+    # train agent2
+    env = EnvCartPole(discrete_base=8, new_step_api=False)
+    agent2 = QAgent(env, n_episodes=300, n_steps=1000, gamma=0.99, alpha=0.5, epsilon=None,
+                    epsilon_start=0.5, epsilon_end=0.0001, epsilon_decay=1.0e+04, )
+    cp2 = "policy2"
+    agent2.run(new_r=False)
+    agent2.visualization()
+
+    # test agent1+agent2
+    agent1 = QAgent(env, n_episodes=1000, epsilon=1)
+    test1 = agent1.test(new_r=False)
+
+    agent2 = QAgent(env, n_episodes=1000, epsilon=None)
+    test2 = agent2.test(new_r=False)
+
+    # plot and compare them
+    df = pd.DataFrame({"Random Baseline": test1[-1000:], "Smart Agent": test2[-1000:]})
+    df = df.stack().reset_index()
+    df.columns = ["x", "hue", "y"]
+
+    agent2.visualization(last_n_steps=-1000, outside_df=df)
